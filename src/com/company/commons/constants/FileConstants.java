783aac462ee2ece3e98b27a701a0a5b06dd9d1c2
Narek Tovmasyan <narektom333@gmail.com>
2021-12-15 22:33:30 +0400

    Briefs

---
+++ src/com/company/commons/constants/FileConstants.java
@@ -0,0 +1,8 @@
+package com.company.commons.constants;
+
+public class FileConstants {
+    public static final String IMAGE_RESOURCE_PATH = "./source/Img/";
+    public static final String WHERE_PATH = "/Desktop";
+    public static final String FOLDER_NAME = "html-css\\RELQ\\CV\\BriefsCv\\fileJSV2";
+    public static final String USER_CV_DATA = "data.js";
+}
